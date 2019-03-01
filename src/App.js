43d721b5d77c0a60ef66208b16ebb58d5ba6869a
Jurian Vink <0950140@hr.nl>
2019-03-01 14:28:35 +0100

    initial commit, created front page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './Header/Header';
+import planet from './planet.png';
+import satellite from './satellite.png';
+import spaceship from './spaceship.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <Header/>
+        <div className="front-page-wrapper">
+          <div className="introduction">
+            <span style={{fontSize: 26, marginBottom: '1em'}}>Welcome! My name is Jurian Vink,</span>
+            <span style={{fontSize: 20, lineHeight: '30px', marginLeft: '.5em'}}>I am a passionate software developer with a sense of design
+            and a passion for making valuable, (re)usable and beautiful software!</span>
+
+            <div className="button-container">
+              <button>Get to know me more</button>
+              <button>Check out my projects</button>
+            </div>
+          </div>
+          <div className="content">
+            <div id="circle-orbit-container">
+              <img src={planet} className="planet" alt="planet" />
+              <div id="inner-orbit">
+                <div class="inner-orbit-cirlces"></div>
+                <img src={spaceship} className="inner-orbit-cirlces" alt="satellite" style={{width: '2em', height: 'auto'}}/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Header/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import earth from './earth.png';
+import './header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <a className="link-no-style left" href="/">
+                    <img src={earth} className="earth" alt="earth" />
+                    <span>Jurian Vink</span>
+                </a>
+                <div className="right">
+                    <ul className="nav-wrapper">
+                        <a className="link-no-style" href="/about"><span className="nav-link">About me</span></a>
+                        <a className="link-no-style" href="/projects"><span className="nav-link">My Projects</span></a>
+                        <a className="link-no-style" href="/contact"><span className="nav-link">Contact</span></a>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
