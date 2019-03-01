57bdac7268c0119f4f97ac6c4028f00b23548770
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-03-01 22:51:56 +0100

    i dont have time for this

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import './Projects.css';
+import projects from './projects.json';
+
+//https://drive.google.com/uc?export=view&id={fileId}
 
 class Projects extends Component {
     render() {
         return (
             <div className="content-wrapper">
                 <span className="title-text">Some projects I worked on</span>
-                <div>
-                    {this.getProjects}
+                <div className="projects-wrapper">
+                    {projects.map((project) => {
+                        return (
+                            <div className="wrapper">
+                                <img className="image" src={project.coverImage}/>
+                                <b>{project.name}</b>
+                                <p>{project.shortDescription}</p>
+                                <button className="button-reversed">Read more</button>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         );
-    }
-
-    getProjects() {
-        let projects = [];
-
-        return projects;
     }
 }
 
