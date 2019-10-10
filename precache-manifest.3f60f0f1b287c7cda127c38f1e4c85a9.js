8b4e59cf27871d395556a210055a3b81594de144
Jurian <j.vink@gdksoftware.nl>
2019-10-10 22:04:22 +0200

    Updates

---
+++ precache-manifest.3f60f0f1b287c7cda127c38f1e4c85a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3d52e63d776277ae67c99690a58223",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71776d972d5c8a1dbbf0",
+    "url": "/portfolio/static/css/2.da5300f4.chunk.css"
+  },
+  {
+    "revision": "2ba04797c6396ad0233a",
+    "url": "/portfolio/static/css/main.01912453.chunk.css"
+  },
+  {
+    "revision": "71776d972d5c8a1dbbf0",
+    "url": "/portfolio/static/js/2.8880fa6f.chunk.js"
+  },
+  {
+    "revision": "2ba04797c6396ad0233a",
+    "url": "/portfolio/static/js/main.c2413fd5.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  }
+]);
